dc207cb0d3be35a69d47580e2aac278b4af33ebf
Julio-at-377165117886 <julio@movanto.com.br>
2024-01-16 17:02:41 -0300

    Criado request para a API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-console.log(`hello world`)
+let offset = 0;
+const limit = 10;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}.`
+
+
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error(error))
